9b3682b444c4befd713891a3524870fe3d9c934c
Depeika <mishra.a.deepika@gmail.com>
2017-08-23 04:23:24 +0000

    Rearrange

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class ntp::params {
   $package_ensure = 'present'
   $config_name = 'ntp.conf'
   $config_file_mode = '0664'
-  $server = ['0.centos.pool.ntp.org', '1.centos.pool.ntp.org']
+  $servers = ['0.centos.pool.ntp.org', '1.centos.pool.ntp.org']
   $service_ensure = 'running'
   $service_enable = true
   $service_hasrestart = true
